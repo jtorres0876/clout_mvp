6b73e5560913f3bd7a4fdbc47f2505997fbefcbc
Jesus Torres <jtorres0876@gmail.com>
2013-04-16 15:42:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloutMvp::Application.config.secret_token = '1da570eb4cd219b039d66516e217ba52c7f4e88a6e023a8e6406461d7e515b9a5af464f432b51e1218549734e9518a3371378ef1bdd13297a778e73013eb9c12'
